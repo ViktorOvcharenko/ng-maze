2f40ce8bac453be360e3ea68154fa5866423d86d
Viktor Ovcharenko <vikt.ovcharenko@gmail.com>
2020-06-29 12:48:59 +0300

    added styles for drawer

--- src/app/core/components/header/header.component.ts
+++ src/app/core/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { MatDrawer } from "@angular/material/sidenav";
 
 @Component({
   selector: 'app-header',
@@ -6,7 +7,7 @@ import { Component, Input } from '@angular/core';
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent {
-  @Input() drawer;
+  @Input() drawer: MatDrawer;
 
   public drawerToggle(): void {
     this.drawer.toggle();
