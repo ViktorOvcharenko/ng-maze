26b00539c62809e42c69b49ae7cb8de5ad859471
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-09-22 22:59:31 +0300

    created maze.selectors.spec

--- src/app/core/store/selectors/account.selectors.spec.ts
+++ src/app/core/store/selectors/account.selectors.spec.ts
@@ -1,16 +1,16 @@
-import { initialAppState } from '../state/app.state';
 import { initialAccountState } from '../state/account.state';
+import { initialAppState } from '../state/app.state';
 import { getAccountLang, getUserName } from './account.selectors';
 
 describe('AccountSelectors', () => {
-  const initialState = { ...initialAccountState };
   const appState = { ...initialAppState };
+  const initialState = { ...initialAccountState };
 
   it('should get the lang', () => {
     expect(getAccountLang(appState)).toBe(initialState.lang);
   });
 
   it('should get the userName', () => {
-    expect(getUserName(appState)).toBe(initialState.lang);
+    expect(getUserName(appState)).toBe(initialState.userName);
   });
 });
