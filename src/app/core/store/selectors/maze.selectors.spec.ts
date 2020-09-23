3e6d8eda93ee696587d05f65413d696bd4785246
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-09-24 00:23:50 +0300

    start testing maze reducers

--- src/app/core/store/selectors/maze.selectors.spec.ts
+++ src/app/core/store/selectors/maze.selectors.spec.ts
@@ -10,31 +10,31 @@ import {
 } from './maze.selectors';
 
 
-describe('AccountSelectors', () => {
+describe('MazeSelectors', () => {
   const appState = { ...initialAppState };
   const initialState = { ...initialMazeState };
 
-  it('should get the levelMode', () => {
+  it('should get the level mode', () => {
     expect(getLevelMode(appState)).toBe(initialState.levelMode);
   });
 
-  it('should get the levelMode', () => {
+  it('should get the hero mode', () => {
     expect(getHeroMode(appState)).toBe(initialState.heroMode);
   });
 
-  it('should get the levelMode', () => {
+  it('should get the wall mode', () => {
     expect(getWallMode(appState)).toBe(initialState.wallMode);
   });
 
-  it('should get the levelMode', () => {
+  it('should get the score', () => {
     expect(getScore(appState)).toBe(initialState.score);
   });
 
-  it('should get the levelMode', () => {
+  it('should get the win', () => {
     expect(getWin(appState)).toBe(initialState.isWin);
   });
 
-  it('should get the levelMode', () => {
+  it('should get the records', () => {
     expect(getRecords(appState)).toBe(initialState.records);
   });
 });
