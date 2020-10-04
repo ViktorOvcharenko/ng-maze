0791ccf712d154a6b15fec3b3bb59f66bf6926f3
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-04 23:57:30 +0300

    update selectors

--- src/app/core/store/selectors/account.selectors.ts
+++ src/app/core/store/selectors/account.selectors.ts
@@ -1,8 +1,8 @@
-import { createSelector } from '@ngrx/store';
+import { createFeatureSelector, createSelector } from '@ngrx/store';
 
 import * as fromModels from '../../models';
 
-const selectAccount = (state: fromModels.IAppState) => state.account;
+const accountFeatureSelector = createFeatureSelector<fromModels.IAppState, fromModels.IAccountState>('account');
 
-export const getAccountLang = createSelector(selectAccount, (state: fromModels.IAccountState) => state.lang);
-export const getUserName = createSelector(selectAccount, (state: fromModels.IAccountState) => state.userName);
+export const getAccountLang = createSelector(accountFeatureSelector, (state: fromModels.IAccountState) => state.lang);
+export const getUserName = createSelector(accountFeatureSelector, (state: fromModels.IAccountState) => state.userName);
