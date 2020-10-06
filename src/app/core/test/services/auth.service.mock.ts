b8299b5c93a2a780e426d12b77399bfc2ec0a947
Ovcharenko <vikt.ovcharenko@gmail.com>
2020-10-06 23:01:58 +0300

    finish testing effects and NavbarComponent

--- src/app/core/test/services/auth.service.mock.ts
+++ src/app/core/test/services/auth.service.mock.ts
@@ -3,5 +3,6 @@ import { of } from 'rxjs';
 export class AuthServiceMock {
   public isAuthenticated = () => false;
   public login = () => of(null);
+  public logout = () => null;
   public signUp = () => of(null);
 }
